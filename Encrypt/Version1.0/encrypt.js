2024a3f1340dac3cf785e3f6bbd9acea369d79a6
LPNeil <42649018+LPNeil@users.noreply.github.com>
2018-08-27 09:30:31 -0600

    Update encrypt.js

--- Encrypt/Version1.0/encrypt.js
+++ Encrypt/Version1.0/encrypt.js
@@ -1,6 +1,10 @@
 var cc1 = ["097","098","099","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122"];
 var EC = {
  e: function(id){
-  alert("Hello id is "+ id);
+  var node = document.getElementById(id).nodeType;
+  if(node=="INPUT"){
+   var nVal = document.getElementById(id).value;
+   encrypt(nVal);
+  }
  }
 };
