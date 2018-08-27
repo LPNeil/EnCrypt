cced5f92451df5c8bb5bae78c25864748362d213
LPNeil <42649018+LPNeil@users.noreply.github.com>
2018-08-27 10:06:08 -0600

    Update encrypt.js

--- Encrypt/Version1.0/encrypt.js
+++ Encrypt/Version1.0/encrypt.js
@@ -1,7 +1,9 @@
 var cc1 = ["097","098","099","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122"];
 var result = "";
 function encrypt(data){
- alert(data + " is the string");
+ for(var i = 0;i<data.length;i++){
+  alert("string is "+i+" in length");
+ }
 }
 var ec = new Object;
 ec.e = function(id){
