71f8cf2c70cfc20514140ba222bc7e3e07a8b1e1
LPNeil <42649018+LPNeil@users.noreply.github.com>
2018-08-27 10:14:20 -0600

    Update encrypt.js

--- Encrypt/Version1.0/encrypt.js
+++ Encrypt/Version1.0/encrypt.js
@@ -2,8 +2,34 @@ var cc1 = ["097","098","099","100","101","102","103","104","105","106","107","10
 var result = "";
 function encrypt(data){
  for(var i = 0;i<data.length;i++){
+  if(data[i]=="A"){result += cc1[0];}
+  if(data[i]=="B"){result += cc1[1];}
+  if(data[i]=="C"){result += cc1[2];}
+  if(data[i]=="D"){result += cc1[3];}
+  if(data[i]=="E"){result += cc1[4];}
+  if(data[i]=="F"){result += cc1[5];}
+  if(data[i]=="G"){result += cc1[6];}
+  if(data[i]=="H"){result += cc1[7];}
+  if(data[i]=="I"){result += cc1[8];}
+  if(data[i]=="J"){result += cc1[9];}
+  if(data[i]=="K"){result += cc1[10];}
+  if(data[i]=="L"){result += cc1[11];}
+  if(data[i]=="M"){result += cc1[12];}
+  if(data[i]=="N"){result += cc1[13];}
+  if(data[i]=="O"){result += cc1[14];}
+  if(data[i]=="P"){result += cc1[15];}
+  if(data[i]=="Q"){result += cc1[16];}
+  if(data[i]=="R"){result += cc1[17];}
+  if(data[i]=="S"){result += cc1[18];}
+  if(data[i]=="T"){result += cc1[19];}
+  if(data[i]=="U"){result += cc1[20];}
+  if(data[i]=="V"){result += cc1[21];}
+  if(data[i]=="W"){result += cc1[22];}
+  if(data[i]=="X"){result += cc1[23];}
+  if(data[i]=="Y"){result += cc1[24];}
+  if(data[i]=="Z"){result += cc1[25];}
  }
- alert("string is "+i+" in length");
+ 
 }
 var ec = new Object;
 ec.e = function(id){
