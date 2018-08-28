f28b788cf8cc2e1998aedd8e2fbd2e869f503823
LPNeil <42649018+LPNeil@users.noreply.github.com>
2018-08-28 08:58:52 -0600

    Update encrypt.js

--- Encrypt/Version1.0/encrypt.js
+++ Encrypt/Version1.0/encrypt.js
@@ -1,41 +1,48 @@
 var cc1 = ["097","098","099","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118","119","120","121","122"];
-var dcc1 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var result = "";
 function encrypt(data){
  for(var i = 0;i<data.length;i++){
-  if(data[i]=="A"){result += cc1[0];}
-  if(data[i]=="B"){result += cc1[1];}
-  if(data[i]=="C"){result += cc1[2];}
-  if(data[i]=="D"){result += cc1[3];}
-  if(data[i]=="E"){result += cc1[4];}
-  if(data[i]=="F"){result += cc1[5];}
-  if(data[i]=="G"){result += cc1[6];}
-  if(data[i]=="H"){result += cc1[7];}
-  if(data[i]=="I"){result += cc1[8];}
-  if(data[i]=="J"){result += cc1[9];}
-  if(data[i]=="K"){result += cc1[10];}
-  if(data[i]=="L"){result += cc1[11];}
-  if(data[i]=="M"){result += cc1[12];}
-  if(data[i]=="N"){result += cc1[13];}
-  if(data[i]=="O"){result += cc1[14];}
-  if(data[i]=="P"){result += cc1[15];}
-  if(data[i]=="Q"){result += cc1[16];}
-  if(data[i]=="R"){result += cc1[17];}
-  if(data[i]=="S"){result += cc1[18];}
-  if(data[i]=="T"){result += cc1[19];}
-  if(data[i]=="U"){result += cc1[20];}
-  if(data[i]=="V"){result += cc1[21];}
-  if(data[i]=="W"){result += cc1[22];}
-  if(data[i]=="X"){result += cc1[23];}
-  if(data[i]=="Y"){result += cc1[24];}
-  if(data[i]=="Z"){result += cc1[25];}
+  if(data[i]=="a"){result += cc1[0];}
+  if(data[i]=="b"){result += cc1[1];}
+  if(data[i]=="c"){result += cc1[2];}
+  if(data[i]=="d"){result += cc1[3];}
+  if(data[i]=="e"){result += cc1[4];}
+  if(data[i]=="f"){result += cc1[5];}
+  if(data[i]=="g"){result += cc1[6];}
+  if(data[i]=="h"){result += cc1[7];}
+  if(data[i]=="i"){result += cc1[8];}
+  if(data[i]=="j"){result += cc1[9];}
+  if(data[i]=="k"){result += cc1[10];}
+  if(data[i]=="l"){result += cc1[11];}
+  if(data[i]=="m"){result += cc1[12];}
+  if(data[i]=="n"){result += cc1[13];}
+  if(data[i]=="o"){result += cc1[14];}
+  if(data[i]=="p"){result += cc1[15];}
+  if(data[i]=="q"){result += cc1[16];}
+  if(data[i]=="r"){result += cc1[17];}
+  if(data[i]=="s"){result += cc1[18];}
+  if(data[i]=="t"){result += cc1[19];}
+  if(data[i]=="u"){result += cc1[20];}
+  if(data[i]=="v"){result += cc1[21];}
+  if(data[i]=="w"){result += cc1[22];}
+  if(data[i]=="x"){result += cc1[23];}
+  if(data[i]=="y"){result += cc1[24];}
+  if(data[i]=="z"){result += cc1[25];}
  }
  return result;
 }
 function decrypt(data){
  for(var i = 0;i<data.length;i++){
-  var code = i+/3;
-  result = code;
+  if(data[0]=="0"){
+   if(data[1]=="9"){
+    if(data[2]=="7"){result += "a";}
+    if(data[2]=="8"){result += "b";}
+    if(data[2]=="9"){result += "c";}
+   }
+  }else{
+   if(data[0]=="1"){
+   }
+  }
  }
  return result;
 }
